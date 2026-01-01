c292e7da70032e484676a05a47ba90a4854a9ed6
Fajla Rabby <fajlarabbyla24@gmail.com>
2025-12-31 19:15:39 -0500

    22-9 Updating and deleting records

--- crud.ts
+++ crud.ts
@@ -30,22 +30,66 @@ async function run() {
 
   // console.log(createProfile);
 
-  const users = await prisma.user.findMany({
-    // ? include means -> add
-    // include: {
-    //   posts: true,
-    //   profile: true,
-    // },
-    // ? select means -> only
-    select: {
-      id: true,
-      name: true,
-      email: true,
+  // const users = await prisma.user.findMany({
+  //   // ? include means -> add
+  //   // include: {
+  //   //   posts: true,
+  //   //   profile: true,
+  //   // },
+  //   // ? select means -> only
+  //   select: {
+  //     id: true,
+  //     name: true,
+  //     email: true,
+  //     posts: true,
+  //     profile: true,
+  //   },
+  // });
+  // console.dir(users, { depth: Infinity });
+
+  // const users = await prisma.profile.findMany();
+  // console.log(users);
+
+  // const updateProfile = await prisma.profile.update({
+  //   where: {
+  //     userId: 1,
+  //   },
+  //   data: {
+  //     bio: "we learn Level 1 web development in PH",
+  //     dateOfBirth: "2025-12-31T00:42:35.381Z",
+  //   },
+  //   select: {
+  //     id: true,
+  //     bio: true,
+  //     user: {
+  //       select: {
+  //         name: true,
+  //         email: true,
+  //         role: true,
+  //       },
+  //     },
+  //   },
+  // });
+
+  // console.log(updateProfile);
+
+  // const deleteUser = await prisma.user.delete({
+  //   where: {
+  //     id: 2,
+  //   },
+  // });
+  // console.log(deleteUser);
+
+  const getUserDataById = await prisma.user.findUnique({
+    where: {
+      id: 2,
+    },
+    include: {
       posts: true,
       profile: true,
     },
   });
-  console.dir(users, { depth: Infinity });
+  console.log(getUserDataById);
 }
 
 run();
